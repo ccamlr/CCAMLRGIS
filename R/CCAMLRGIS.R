d4083b1423f721bb3ebb2d22d7e38fa2dbf9ca82
CCAMLR-Science <science@ccamlr.org>
2019-11-18 11:49:39 +1100

    Documentation updates

--- R/CCAMLRGIS.R
+++ R/CCAMLRGIS.R
@@ -1,103 +1,36 @@
-#' CCAMLRGIS: A package to load and create spatial data, including layers and tools that are relevant to CCAMLR activities.
+#' A package to load and create spatial data, including layers and tools that are relevant to CCAMLR activities.
 #'
 #' The CCAMLRGIS package provides two broad categories of functions: load functions and create functions.
 #' 
 #' @section Load functions:
 #' Load functions are used to import CCAMLR geo-referenced layers and include:
-#' load_ASDs, load_SSRUs, load_RBs, load_SSMUs, load_MAs, load_Coastline, load_RefAreas, load_MPAs, and load_EEZs.
+#' \itemize{
+#'   \item \link{load_ASDs}
+#'   \item \link{load_SSRUs}
+#'   \item \link{load_RBs}
+#'   \item \link{load_SSMUs}
+#'   \item \link{load_MAs}
+#'   \item \link{load_Coastline}
+#'   \item \link{load_RefAreas}
+#'   \item \link{load_MPAs}
+#'   \item \link{load_EEZs}
+#' }
 #' 
 #' @section Create functions:
 #' Create functions are used to create geo-referenced layers from user-generated data and include:
-#' create_Points, create_Lines, create_Polys, create_PolyGrids, create_Stations and create_RefGrid
-#'  
+#' \itemize{
+#'   \item \link{create_Points}
+#'   \item \link{create_Lines}
+#'   \item \link{create_Polys}
+#'   \item \link{create_PolyGrids}
+#'   \item \link{create_Stations}
+#'   \item \link{create_RefGrid}
+#' }
+#' 
 #' @section Vignette:
 #' To learn more about CCAMLRGIS, start with the vignette:
 #' \code{browseVignettes(package = "CCAMLRGIS")}
 #' 
-#' 
 #' @docType package
 #' @name CCAMLRGIS
-#' @importFrom graticule graticule
 NULL
-
-#' ASD data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name ASD_data
-NULL
-
-#' SSRU data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name SSRU_data
-NULL
-
-#' SSMU data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name SSMU_data
-NULL
-
-
-#' Coastline data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Coastlines
-#' @name Coastline_data
-NULL
-
-#' MA data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name MA_data
-NULL
-
-
-#' MPA data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name MPA_data
-NULL
-
-
-#' RB data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/home see information (i) under Management areas
-#' @name RB_data
-NULL
-
-#' RefAreas 
-#'  
-#'  These were Reference Areas used in local toothfish biomass estimation (see WG-FSA-17/42 for further details)
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source see inst/lrobinson-notes/createRefAreas.R
-#' @name RefAreas
-NULL
-
-
-#' EEZ data
-#'  
-#' @docType data
-#' @format SpatialPolygonsDataFrame
-#' @source https://gis.ccamlr.org/ see information (i) under Management areas
-#' @name EEZ_data
-NULL
-
-
-
-
---
+++ R/CCAMLRGIS_DataDescription.R
@@ -0,0 +1,113 @@
+#' CCAMLRGIS Projection
+#'
+#' The CCAMLRGIS package uses the \href{https://en.wikipedia.org/wiki/Lambert_azimuthal_equal-area_projection}{Lambert azimuthal equal-area projection}.
+#'
+#' @docType data
+#' @format Character string: "+proj=laea +lat_0=-90 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
+#' @source \url{https://gis.ccamlr.org/}
+#' @name CCAMLRp
+NULL
+
+#' Coast
+#'
+#' Coastline polygons generated from \link{load_Coastline} and sub-sampled to only contain data that falls
+#' within the CCAMLR boundaries (contour of ASDs + EEZs).
+#'
+#' @docType data
+#' @format SpatialPolygons
+#' @usage plot(Coast,col='grey')
+#' @source \url{https://gis.ccamlr.org/}
+#' @name Coast
+NULL
+
+#' Bathymetry colors
+#'
+#' Set of standard colors to plot bathymetry, to be used in conjunction with \link{Depth_cuts}.
+#'
+#' @docType data
+#' @format Character vector
+#' @usage plot(SmallBathy,breaks=Depth_cuts,col=Depth_cols,axes=F,box=F)
+#' @name Depth_cols
+NULL
+
+#' Bathymetry depth classes
+#'
+#' Set of depth classes to plot bathymetry, to be used in conjunction with \link{Depth_cols}.
+#'
+#' @docType data
+#' @format Numeric vector
+#' @usage plot(SmallBathy,breaks=Depth_cuts,col=Depth_cols,axes=F,box=F)
+#' @name Depth_cuts
+NULL
+
+#' Example dataset for create_PolyGrids
+#'
+#' To be used in conjunction with \link{create_PolyGrids}.
+#'
+#' @docType data
+#' @format DataFrame
+#' @usage 
+#' View(GridData)
+#' 
+#' MyGrid=create_PolyGrids(Input=GridData,dlon=2,dlat=1)
+#' plot(MyGrid,col=MyGrid$Col_Catch_sum)
+#' @name GridData
+NULL
+
+#' Example dataset for create_Lines
+#'
+#' To be used in conjunction with \link{create_Lines}.
+#'
+#' @docType data
+#' @format DataFrame
+#' @usage 
+#' View(LineData)
+#' 
+#' MyLines=create_Lines(LineData)
+#' plot(MyLines,lwd=2)
+#' @name LineData
+NULL
+
+#' Example dataset for create_Points
+#'
+#' To be used in conjunction with \link{create_Points}.
+#'
+#' @docType data
+#' @format DataFrame
+#' @usage 
+#' View(PointData)
+#' 
+#' MyPoints=create_Points(PointData)
+#' plot(MyPoints)
+#' text(MyPoints$x,MyPoints$y,MyPoints$name,adj=c(0.5,-0.5),xpd=T)
+#' plot(MyPoints[MyPoints$name=='four',],bg='red',pch=21,cex=1.5,add=T)
+#' @name PointData
+NULL
+
+#' Example dataset for create_Polys
+#'
+#' To be used in conjunction with \link{create_Polys}.
+#'
+#' @docType data
+#' @format DataFrame
+#' @usage 
+#' View(PolyData)
+#' 
+#' MyPolys=create_Polys(PolyData,Densify=T)
+#' plot(MyPolys,col='green',add=T)
+#' text(MyPolys$Labx,MyPolys$Laby,MyPolys$ID)
+#' plot(MyPolys[MyPolys$ID=='three',],border='red',lwd=3,add=T)
+#' @name PolyData
+NULL
+
+#' Small bathymetry dataset
+#'
+#' Bathymetry dataset derived from the \href{https://www.gebco.net/}{GEBCO 2019} dataset.
+#' Subsampled using raster's \link[raster]{resample} function, using the nearest neighbor method
+#' and a 2500m resolution. \strong{Not to be used for accurate bathymetry analyses}.
+#'
+#' @docType data
+#' @format raster
+#' @usage plot(SmallBathy,breaks=Depth_cuts,col=Depth_cols,axes=F,box=F)
+#' @name SmallBathy
+NULL
